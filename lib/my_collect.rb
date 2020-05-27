c5118347b32a08ff622407187b0cfc739b3fe4de
Daniel Andrew <danwandrew@gmail.com>
2020-05-27 10:19:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(array)
+  if block_given?
+    i=0;
+    collection =[]
+    while i<array.length;
+    collection << yield(array[i].split(' ').[0])
+  else "No block given"
+end
+end
 
